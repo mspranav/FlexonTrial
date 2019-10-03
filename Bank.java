63d26edea68068e3314872f5baa14afb4f3fc8f6
mspranav <smandad1@asu.edu>
2019-10-03 14:35:37 -0700

    Bank App everything working except comparision with current localtime

--- Bank.java
+++ Bank.java
@@ -6,42 +6,72 @@ public class Bank{
 	float balance;
 	String email = new String("");
 	int phonenumber;
-	int depositnumber = 0;
+	int depositnumber;
+	int withdrawalamt;
+	// String [] transaction_history;
+	List<String [] > transaction_history = new ArrayList<String []>();  
+
 	
-	public void Bank(){
+	public Bank(){
 		accnumber = 123456789;
 		balance = (float) 50.0;
 		customername = "mspranav";
 		email = "mspranav@gmail.com";
-		phonenumber = 101;	
+		phonenumber = 101;
+		depositnumber = 0;	
+		withdrawalamt = 1000;
+
 	}
 	
-	public String deposit(float amt) {
-		if (amt > 50 && amt < 500 && depositnumber <= 3) {
+	public void deposit(float amt) {
+		if (amt > 50 && amt <= 500 && depositnumber < 3) {
+			System.out.println("\n");
 			balance += amt;
 			depositnumber += 1;
-			return "Deposited";
-			
+			transaction_history.add(new String []{"Deposit" , Float.toString(amt)});
+
+			//return "Deposited";
+			//System.out.println(transaction_history);
 		}
-		else {
-			System.out.println("Error Occureed w.r.t. amt");
+		else if(depositnumber >= 3) {
+			System.out.println("You have reached the Limit for # of Transactions\n");
 		}
-		
+		else{
+			System.out.println("Error Occureed w.r.t. amt\n");
+		}
+		//return "Testing";	
 	}
 	
-	public int withdraw(float amt) {
-		if (amt <= balance && amt <= 1000) {
+	public void withdraw(float amt) {
+		if (amt <= balance && amt <= 1000 && withdrawalamt > 0){ //&& balance - amt <= withdrawalamt) {
 			balance -= amt;
+			withdrawalamt -= amt;
+			System.out.println("You can still withdraw " + withdrawalamt + " today"); 
+			transaction_history.add(new String []{"Withdraw" , Float.toString(amt)});
 		}
-		else {
-			System.out.print("Insufficient Funds");
+		else if (withdrawalamt == 0){
+			System.out.print("Withdraw Limit Per Day - Reached\n");
+
 		}
+		else{
+			System.out.println("Insufficient Funds or Withdrawal amount Limit Reached \n");
+		}
+	//return balance;
 	}
 	
-	public void getBalance() {
+	public void getTransactionHistory(){
+		System.out.println("\n\n");
+		for(String [] transaction: transaction_history  ) {
+			System.out.println(Arrays.toString(transaction));
+		}
+	}
+
+	public void  getBalance() {
 //		return
-		System.out.println(customername);
+		System.out.print(customername +'\n');
+		System.out.println("Type : Checking");
 		System.out.println(balance);
+		System.out.println(java.time.LocalDate.now() +"  " + java.time.LocalTime.now());
 		
 	}
 	
--- BankMain.java
+++ BankMain.java
@@ -1,4 +1,5 @@
 import java.lang.String;
+import java.lang.System;
 import java.util.*;
 
 public class BankMain extends  Bank{
@@ -9,23 +10,51 @@ public class BankMain extends  Bank{
 	}
 	
 	public static void main(String args[]) {
+		float amt;
 		Bank obj = new Bank();
+		//System.out.print(obj.customername);
+		//obj.getBalance();//System.out.println(obj.getBalance());
 		while(true) {
+			System.out.println("\n \n ---------------------------------------------- \n \n");
 			System.out.println("Select the Operations from below");
 			System.out.println("1.Deposit");
 			System.out.println("2.Withdraw");
 			System.out.println("3.Show Balance");
-			System.out.println("4.Exit");
+			System.out.println("4.Get Transaction History");
+			System.out.println("5.Exit");
 //			System.out.println("5.Create Account"); //create another object 
 			System.out.println("Your choice");
 			Scanner scnobj = new Scanner(System.in);
 			if (scnobj.hasNextInt() == true) {
 				int userchoice = scnobj.nextInt();
 				//switch case
+				switch(userchoice){
+
+					case 1: System.out.println("Enter Amount to be Deposited");
+							amt = scnobj.nextFloat();
+							obj.deposit(amt);
+							//obj.getBalance();
+							break;
+					case 2: System.out.println("Enter Amount you wish to be withdrawn");
+							amt = scnobj.nextFloat();
+							obj.withdraw(amt);
+							//obj.getBalance();
+							break;
+					case 3: System.out.println("\n\nYour Balance:");
+							obj.getBalance();// + (String)obj.getBalance() );
+							break;
+					case 4: obj.getTransactionHistory();
+							break;
+
+					case 5: System.exit(0);
+
+					default: System.out.println("Please enter a valid choice");
+				}
 			}
 			
-			
+			//System.out.println("testing");
 			
 		}
+		
 	}
 }
